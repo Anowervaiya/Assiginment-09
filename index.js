428d155c6b691e9a7b9ccbcfe5b620379366ef13
Anowervaiya <mdanowerhossen727@gmail.com>
2024-05-14 18:24:57 +0600

    project little bit done

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const cors = require('cors');
 const port = process.env.port || 3000;
 const dotenv = require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 app.use(
   cors({
     origin: [
@@ -26,15 +28,28 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+const Varify = async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return res.status(401).send({ messege: 'unathorized' });
+  }
 
+  jwt.verify(token, process.env.SECRET, async (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ messege: 'unathorized' });
+    }
+    req.user = decoded;
+
+    next();
+  });
+};
 async function run() {
   try {
     // await client.connect();
     const database = client.db('GroupStudy');
     const Assignment = database.collection('Assignment');
-    const SubmitAssignment = database.collection('SubmitAssignment')
-    
-    
+    const SubmitAssignment = database.collection('SubmitAssignment');
+
     app.post('/assignment', async (req, res) => {
       const { newAssignment } = req.body;
       const doc = {
@@ -53,18 +68,18 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/allassignment', async (req, res) => {
+    app.get('/allassignment',  async (req, res) => {
+     
       const cursor = await Assignment.find();
-    
+
       const result = await cursor.toArray();
 
       res.send(result);
-
     });
     app.delete('/delete', async (req, res) => {
       const Id = req.query.id;
       const email = req.query.email;
-   
+
       const query = {
         _id: new ObjectId(Id),
 
@@ -76,7 +91,6 @@ async function run() {
 
     app.get('/details', async (req, res) => {
       const Id = req.query.id;
-     
 
       const query = { _id: new ObjectId(Id) };
       const result = await Assignment.find(query).toArray();
@@ -109,13 +123,13 @@ async function run() {
       const SubmitedValue = req.body;
       const Id = req.query.id;
       const query = {
-        _id: new ObjectId(Id)
-      }
+        _id: new ObjectId(Id),
+      };
       const updateDoc = {
         $set: {
           Status: 'Marked',
           Number: SubmitedValue?.Number,
-          FeedBack: SubmitedValue?.Feedback
+          FeedBack: SubmitedValue?.Feedback,
         },
       };
       const options = { upsert: true };
@@ -124,58 +138,86 @@ async function run() {
         updateDoc,
         options
       );
-      res.send(result)
-
-    })
+      res.send(result);
+    });
 
     app.get('/pending', async (req, res) => {
       const query = {
-        Status:'pending'
-      }
-      const cursor = await SubmitAssignment.find(query)
-      const result = await cursor.toArray()
-    
+        Status: 'pending',
+      };
+      const cursor = await SubmitAssignment.find(query);
+      const result = await cursor.toArray();
+
       res.send(result);
-    })
+    });
     app.get('/pending/:id', async (req, res) => {
       const Id = req.params.id;
       const query = {
         Status: 'pending',
-        _id: new ObjectId(Id)
-      }
-      const cursor = await SubmitAssignment.find(query)
-      const result = await cursor.toArray()
-    
+        _id: new ObjectId(Id),
+      };
+      const cursor = await SubmitAssignment.find(query);
+      const result = await cursor.toArray();
+
       res.send(result);
-    })
+    });
 
     app.post('/Submit', async (req, res) => {
-      const  submit  = req.body;
-    ;
-       const doc = {
-         UserName: submit.UserName,
-         UserEmail: submit.UserEmail,
-         Status: submit.status,
-         File: submit.File,
-         Note: submit.note,
-         UserPhoto: submit.UserPhoto,
-         AssignmentName: submit.AssignmentName,
-         TotalMarks: submit.TotalMarks,
-       };
+      const submit = req.body;
+      const doc = {
+        UserName: submit.UserName,
+        UserEmail: submit.UserEmail,
+        Status: submit.status,
+        File: submit.File,
+        Note: submit.note,
+        UserPhoto: submit.UserPhoto,
+        AssignmentName: submit.AssignmentName,
+        TotalMarks: submit.TotalMarks,
+      };
 
-       const result = await SubmitAssignment.insertOne(doc);
-       res.send(result);
-    })
-    app.get('/attempt/:email', async (req, res) => {
+      const result = await SubmitAssignment.insertOne(doc);
+      res.send(result);
+    });
+    app.get('/attempt/:email',Varify, async (req, res) => {
       const Email = req.params.email;
+       const UserDetails = req.user;
       
+      if (Email != UserDetails?.user) {
+  return res.status(403).send({messege:'forbidden access'})
+}
       const query = {
-        UserEmail: Email
-      }
-      const cursor = await SubmitAssignment.find(query)
-      const result = await cursor.toArray()
-     
-      res.send(result)
+        UserEmail: Email,
+      };
+      const cursor = await SubmitAssignment.find(query);
+      const result = await cursor.toArray();
+
+      res.send(result);
+    });
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+
+      const Token = jwt.sign(user, process.env.SECRET, { expiresIn: '360d' });
+
+      res
+        .cookie('token', Token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production' ? 'true' : 'false',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ messege: 'success' });
+    });
+
+    app.post('/jwtDelete', async (req, res) => {
+      res
+        .clearCookie('token', {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production' ? 'true' : 'false',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          maxAge: 0,
+        })
+
+        .send({ success: true });
     })
 
     // Create a document to insert
